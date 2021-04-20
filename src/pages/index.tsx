568ce8fdb72e5a22d88b17a820b8400f67f8d58f
Luke-Henry-Santos <lukitao.santos@gmail.com>
2021-04-20 13:47:12 -0300

    Initial commit
    # Creation of the main page;
    # Creation of the player;
    # Addition of icons / fonts
    # Use of JSON-SERVER as an API simulator.
    # Small settings.

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+export default function Home(props) {
+  return (
+    <h1>Index</h1>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/episodes')
+  const data = await res.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
